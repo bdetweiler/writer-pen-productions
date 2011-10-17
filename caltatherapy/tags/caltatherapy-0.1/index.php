8b09690ab530b37f3ca31210f461cd3be4e53b1d
bdetweiler <bdetweiler@399476cf-f53c-358c-37ee-cd38a1298b4d>
2011-10-17 23:55:16 +0000

    Tagging first alpha release

---
+++ caltatherapy/tags/caltatherapy-0.1/index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link href="css/main.css" media="screen" rel="stylesheet" type="text/css" />
+        <script type="text/JavaScript" src="js/jquery-1.6.4.min.js" />
+
+        <script type="text/javascript">
+
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-25138124-1']);
+            _gaq.push(['_setDomainName', '.caltatherapy.com']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+
+        </script>
+    </head>
+    <body>
+
+        <!--
+        <div id="background">
+            <img height="1800" width="1800" alt="Calta Therapy" src="images/clouds_background_reduced.jpg">
+        </div>
+        -->
+        <div id="banner" class="banner">
+            <span id="bannerTitle" class="bannerTitle">
+                MaryAnn Calta, <span id="qualifications" class="qualifications">LCSW, LHMP, CMSW, M.Div</span>
+            </span>
+            <br />
+            <span id="bannerFoot" class="bannerFoot">
+                Psychotherapy <span class="diamond">&#x25C6;</span> Spiratual Direction
+            </span>
+
+            <span id="leaf" class="leaf"><img src="images/fall-leaf1.png" /></span>
+        </div>
+        <div id="tabsHolder" class="tabsHolder">
+            <span id="tabAbout" class="tabs" onClick="show('about')">
+                About
+            </span>
+            <span id="tabOffice" class="tabs" onClick="show('office')">
+                Office
+            </span>
+            <span id="tabPayments" class="tabs" onClick="show('payments')">
+                Payments
+            </span>
+            <span id="tabForms" class="tabs" onClick="show('forms')">
+                Forms
+            </span>
+        </div>
+
+        <div id="about" class="contentWrapper">
+            <div id="content" class="content">
+                <img src="images/black_x.png" class="blackx" alt="Close" id="black_x" onClick="hide('about')"/>
+                <div id="picColumn" class="picColumn">
+                    <div class="macImage" align="center">
+                        <img src="images/ma_calta_yellow.jpg" class="macImage" alt="MaryAnn Calta" id="macImage" />
+                    </div>
+                    <br />
+                    <div id="addressText" class="addressText">
+                        <div id="addressHeaderText" class="addressHeaderText">
+                            The Historic Paxton Building
+                        </div>
+                        1403 Farnam St <span class="diamond">&#x25C6;</span> Suite 215
+                        <br />
+                        Omaha, NE 68102
+                        <br />
+                        Office: 402.393.0642
+                        <br />
+                        Fax: 402.391.2641
+                        <br />
+                    </div>
+                </div>
+                <div id="textColumn" class="textColumn">
+                    <p>
+                        &nbsp;&nbsp;&nbsp;&nbsp;MaryAnn Calta LCSW, LHMP, CMSW, M.Div, has more than fifteen years experience as a psychodynamic psychotherapist (psychotherapy) using Jungian, developmental, and other psychodynamic theories. She is trained in systems theory and dynamics, including conflict resolution. Her areas of expertise include grief work, recovery from trauma, depression, anxiety, and sexual violence, including clergy sexual abuse and misconduct. Consultation is offered to professionals in various fields.
+                    </p>
+                    <p>
+                        &nbsp;&nbsp;&nbsp;She began her work in social services in 1972 in the Department of Human Services in California. Through her 39 years of journeying with others, she has become a specialist at integrating mind, body, and spirit for holistic health. Besides her credentials in clinical social work, (Certified Master Social Worker, Licensed Clinical Social Worker, and Licensed Mental Health Practitioner) she is a certified spiritual director, Reiki II practitioner, and is an ordained Presbyterian minister (M.Div).
+                    </p>
+                    <p>
+                        &nbsp;&nbsp;&nbsp;&nbsp;Traditional “Christian” therapy is NOT used in her practice; rather, Ms. Calta draws from her extensive background of working in interfaith settings as clergy and secular clinical settings as a hospital chaplain and psychotherapist. Incorporating any spiritual issues into the therapy is done strictly at the client’s request. She welcomes people of all faith traditions.
+                    </p>
+                    <p>
+                        &nbsp;&nbsp;&nbsp;&nbsp;Ms. Calta believes the common boundaries of spiritual and mental health intersect in our lives as we search for meaning and wholeness, regardless of faith background. Helping people make meaning out of their lives and facilitating psychological healing are her gifts. 
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div id="office" class="contentWrapper">
+            <div id="content" class="content">
+                <img src="images/black_x.png" class="blackx" alt="Close" id="black_x" onClick="hide('office')"/>
+                <div id="picColumn" class="picColumn">
+                    <img src="images/profile_photo.png" />
+                    <br />
+                    <div id="addressText" class="addressText">
+                        <div id="addressHeaderText" class="addressHeaderText">
+                            The Historic Paxton Building
+                        </div>
+                        1403 Farnam St <span class="diamond">&#x25C6;</span> Suite 215
+                        <br />
+                        Omaha, NE 68102
+                        <br />
+                        Office: 402.393.0642
+                        <br />
+                        Fax: 402.391.2641
+                        <br />
+                    </div>
+                </div>
+                <div id="textColumn" class="textColumn">
+                    <p>Office is conveniently located in the Historic Paxton Building in downtown Omaha at the Southwest Corner of 14th and Farnam Streets.</p>
+                    <p>Metered parking is available on either Farnam or 14th Streets.</p>
+                    <iframe width="425"
+                            height="350"
+                            frameborder="0"
+                            scrolling="no"
+                            marginheight="0"
+                            marginwidth="0"
+                            src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=1403+Farnam+Street,+Omaha,+NE&amp;aq=0&amp;sll=37.0625,-95.677068&amp;sspn=43.528905,114.169922&amp;ie=UTF8&amp;hq=&amp;hnear=1403+Farnam+St,+Omaha,+Douglas,+Nebraska+68102&amp;ll=41.257273,-95.935002&amp;spn=0.005065,0.013937&amp;t=h&amp;z=14&amp;output=embed">
+                    </iframe>
+                    <br />
+                    <small>
+                        <a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=1403+Farnam+Street,+Omaha,+NE&amp;aq=0&amp;sll=37.0625,-95.677068&amp;sspn=43.528905,114.169922&amp;ie=UTF8&amp;hq=&amp;hnear=1403+Farnam+St,+Omaha,+Douglas,+Nebraska+68102&amp;ll=41.257273,-95.935002&amp;spn=0.005065,0.013937&amp;t=h&amp;z=14" 
+                           style="color:#0000FF;text-align:left">View Larger Map</a>
+                    </small>
+                </div>
+            </div>
+        </div>
+
+        <div id="payments" class="contentWrapper">
+            <div id="content" class="content">
+                <img src="images/black_x.png" class="blackx" alt="Close" id="black_x" onClick="hide('payments')"/>
+                <div id="picColumn" class="picColumn">
+                    <img src="images/profile_photo.png" />
+                    <br />
+                    <div id="addressText" class="addressText">
+                        <div id="addressHeaderText" class="addressHeaderText">
+                            The Historic Paxton Building
+                        </div>
+                        1403 Farnam St <span class="diamond">&#x25C6;</span> Suite 215
+                        <br />
+                        Omaha, NE 68102
+                        <br />
+                        Office: 402.393.0642
+                        <br />
+                        Fax: 402.391.2641
+                        <br />
+                    </div>
+                </div>
+                <div id="textColumn" class="textColumn">
+                    <p>Most major insurances are accepted, as well as major credit cards.</p>
+                    <img src="images/visa_mastercard_logo.gif" alt="Visa/Mastercard" />
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
+
+<script language="JavaScript">
+
+$(document).ready(function() {
+    hide('all');
+});
+
+function hide(category)
+{
+    if(category == "all")
+    {
+        $('#about').hide('fast');
+        $('#office').hide('fast');
+        $('#payments').hide('fast');
+        $('#forms').hide('fast');
+
+        $('#tabAbout').addClass("tabs");
+        $('#tabAbout').removeClass("tabSelected");
+        $('#tabOffice').addClass("tabs");
+        $('#tabOffice').removeClass("tabSelected");
+        $('#tabPayments').addClass("tabs");
+        $('#tabPayments').removeClass("tabSelected");
+        $('#tabForms').addClass("tabs");
+        $('#tabForms').removeClass("tabSelected");
+    }
+    else if(category == "about")
+    {
+        $('#about').hide('fast');
+        $('#tabAbout').addClass("tabs");
+        $('#tabAbout').removeClass("tabSelected");
+    }
+    else if(category == "office")
+    {
+        $('#about').hide('fast');
+        $('#tabOffice').addClass("tabs");
+        $('#tabOffice').removeClass("tabSelected");
+    }
+    else if(category == "payments")
+    {
+        $('#payments').hide('fast');
+        $('#tabPayments').addClass("tabs");
+        $('#tabPayments').removeClass("tabSelected");
+    }
+    else if(category == "forms")
+    {
+        $('#forms').hide('fast');
+        $('#tabForms').addClass("tabs");
+        $('#tabForms').removeClass("tabSelected");
+    }
+}
+
+function show(category)
+{
+    hide("all");
+
+    if(category == "about")
+    {
+        $('#about').show('slow');
+        $('#tabAbout').addClass("tabSelected");
+        $('#tabAbout').removeClass("tabs");
+    }
+    else if(category == "office")
+    {
+        $('#office').show('slow');
+        $('#tabOffice').addClass("tabSelected");
+        $('#tabOffice').removeClass("tabs");
+    }
+    else if(category == "payments")
+    {
+        $('#payments').show('slow');
+        $('#tabPayments').addClass("tabSelected");
+        $('#tabPayments').removeClass("tabs");
+    }
+    else if(category == "forms")
+    {
+        $('#forms').show('slow');
+        $('#tabForms').addClass("tabSelected");
+        $('#tabForms').removeClass("tabs");
+    }
+
+
+}
+
+/*
+    $('#tab1').click(function() {
+      $('#tab1').fadeOut('slow', function() {
+        // Animation complete.
+      });
+    });
+*/
+
+</script>
